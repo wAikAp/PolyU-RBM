b870d28225faaa6ff0f1a35f13c1204ddad3c91f
kintat1120 <52268674+kintat1120@users.noreply.github.com>
2021-04-03 23:43:11 +0800

    03042021_Update
    1. Read the command
    2. Add booking command(addMeeting,addPresentation,bookDevice,addConference) to "allBooking.log"
    3. Read the batch file
    4. Create child and close the corresponding pipes

--- RBM.c
+++ RBM.c
@@ -3,9 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 
+//<===============================Variables=============================>
 /*Regex*/
 #include <regex.h>
-
+/*First Keyword*/
 const char* ADDMET = "addMeeting";
 const char* ADDPRE = "addPresentation";
 const char* ADDCON = "addConference";
@@ -13,6 +14,32 @@ const char* BOKDEV = "bookDevice";
 const char* ADDBAT = "addBatch";
 const char* PRTBOK = "printBookings";
 const char* ENDPGM = "endProgram";
+
+char input[255];
+char *str;
+/*regx pattern*/
+char *ptnDate = "[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])";
+char *ptnTime = "(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])";
+char ptnKeyWord[100];
+
+//pipes and child
+int child[3];
+int pipes[6][2];
+
+//loop variable
+int i,j;
+//<===============================Variables=============================>
+//<===============================function==============================>
+//Add keyword into array
+void define_keyword(char *ptnKeyWord){
+  strcpy(ptnKeyWord, "(");
+  strcat(ptnKeyWord, ADDMET);
+  strcat(ptnKeyWord, "|");
+  strcat(ptnKeyWord, ADDPRE);
+  strcat(ptnKeyWord, "|");
+  strcat(ptnKeyWord, ADDCON);
+  strcat(ptnKeyWord, ")");
+}
 
 //regex matching
 int match(const char *string, const char *pattern)
@@ -25,55 +52,145 @@ int match(const char *string, const char *pattern)
   return 1;
 }
 
+//Split the command
+char* extract_cmd(char *input){
+  str = strtok (input," ");
+  while (str != NULL) {
+      //printf("%s\n", str);
+      //check pattern date
+      int m = match(str,ptnDate);
+      if(m)
+      {
+        //printf("match date = %s\n", str);
+        m = 0;
+      }
+      //check pattern keyword
+      m = match(str,ptnKeyWord);
+      
+      if(m)
+      {
+        //printf("match keyword = %s\n", str);
+        m = 0;
+      }
+
+      //Get the .dat filename
+      if (strstr(str, ".dat")!=NULL){
+        strcpy(&str[0], &str[0 + 3]);
+        return (str);
+      }
+
+      //find next string
+      str = strtok (NULL, " ");
+  }    
+}
+
+//Read the batch file
+void batchFileHandler(char *filename){
+  FILE *infilep;
+  char line[100];
+  char file[100];
+  strncpy(file, filename, strlen(filename)-1);
+  file[strlen(filename)-1] = '\0';
+  infilep = fopen(file, "r");
+  if (infilep == NULL) {
+    printf("Error in opening input file\n");
+    exit(1);
+  }
+  while (fgets( line, 100, infilep ) != NULL ) {
+    printf("%s",line);
+  }
+}
+//<===============================function==============================>
 
 int main(void) {
-  char input[255];
-  char *str;
-  /*regx pattern*/
-  char *ptnDate = "[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])";
-  char *ptnTime = "(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])";
-  char ptnKeyWord[100];
-  strcpy(ptnKeyWord, "(");
-  strcat(ptnKeyWord, ADDMET);
-  strcat(ptnKeyWord, "|");
-  strcat(ptnKeyWord, ADDPRE);
-  strcat(ptnKeyWord, "|");
-  strcat(ptnKeyWord, ADDCON);
-  strcat(ptnKeyWord, ")");
+  char buf[80]={0};
+  /*
+  //Create new pipe
+  for (i = 0;i < 6; i++){
+    if (pipe(pipes[i]) < 0){
+        printf("pipe creation error\n");
+        exit(1);
+    }
+  }
+
+  //Create Child
+  for(i = 0;i < 3; i++){
+    child[i] = fork();
+    if (child[i]<0) {
+    printf("Fork Failed\n");
+    exit(1);
+    } else if (child[i]==0) {
+      //Close unused pipes in Child
+      for (j = 0; j < 6; j++){
+        if(j != i*2){
+          close(pipes[j][0]);
+        }
+        if(j != (i*2+1)){
+          close(pipes[j][1]);
+        }
+      }
+      read(pipes[i*2][0],buf,80);
+      break;
+    }
+  }
+
+  //Close unused pipes in Parent
+  for (i = 0; i < 6; i++){
+    if(i%2 == 0){
+      close(pipes[i][0]);
+    } else if (i%2 != 0){
+      close(pipes[i][1]);
+    }
+  }
+  */
+
+  FILE *outfilep;
+  define_keyword(ptnKeyWord);
   //printf("ptnKeyWord = %s",ptnKeyWord);
-  
   /*program start*/
   printf("~~ WELCOME TO PolySME ~~\n");
-  // while (1)
+  //Open allBooking.log
+  outfilep = fopen("allBooking.log", "a");
+  if (outfilep == NULL) {
+    printf("Error in opening output file\n");
+    exit(1);
+  }
+
+
+
+  //Get user input
+  while (1)
   {
     printf("Please enter booking:\n");
     // fgets(input, sizeof input, stdin);
     // scanf(" %[^;]",input);
     //get whole line
-    scanf(" %[^;\n]",input);
+    fgets(input,255,stdin);
+    if (strstr(input, "endProgram") != NULL) {
+      remove("allBooking.log");
+      //remove("FCFSBooking.log");
+      //remove("PRIOBooking.log");
+      //remove("OPTIBooking.log");
+      printf("Bye!\n");
+      break;
+    } else if (strstr(input, "addBatch")!=NULL){
+      batchFileHandler(extract_cmd(input));
+    } else if (strstr(input, "printBookings")!=NULL){
+      printf("Check what request\n");
+    } else if (strstr(input, "addMeeting")!=NULL || 
+               strstr(input, "addPresentation")!=NULL ||
+               strstr(input, "addConference")!=NULL ||
+               strstr(input, "bookDevice")!=NULL){
+      fprintf(outfilep, "%s", input);
+    } else {
+      printf("Error, Please try again!\n");
+    }
+    //extract_cmd(input);
     
-    str = strtok (input," ");
-    while (str != NULL) {
-        printf("%s\n", str);
-        //check pattern date
-        int m = match(str,ptnDate);
-        if(m)
-        {
-          printf("match date = %s\n", str);
-          m = 0;
-        }
-        //check pattern keyword
-        m = match(str,ptnKeyWord);
-        if(m)
-        {
-          printf("match keyword = %s\n", str);
-          m = 0;
-        }
-
-        //find next string
-        str = strtok (NULL, " ");
-    }    
   }
-  
+  fclose(outfilep);
+  for(i=0;i<3;i++){
+    wait(NULL);
+  }
   return 0;
 }
